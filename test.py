da3e4ad88eca5f2633b4f0a786a332e3a3cfee45
vvlovsky <slvtrshkv@gmail.com>
2021-06-16 01:03:50 +0200

    bugfixes

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from time import sleep
 
 def generate_transaction():
     result = {}
-    sender_id = ''.join(random.choice(range(10)) for _ in range(16))
+    sender_id = ''.join(str(random.choice(range(10))) for _ in range(16))
     result |= {"sender_id": sender_id}
     x = random.randint(0, 100)
     y = random.randint(0, 100)
@@ -23,28 +23,36 @@ def generate_transaction():
     return result
 
 
-nodes = ['95.179.240.109', '192.248.191.151', '45.76.82.209', '209.250.233.1',
+nodes_clean = ['95.179.240.109', '192.248.191.151', '45.76.82.209', '209.250.233.1',
          '192.248.184.111', '192.248.190.185', '45.63.119.143', '80.240.16.250',
          '95.179.251.26', '192.248.177.110']
+# nodes_clean = ['127.0.0.1', '127.0.0.2', '127.0.0.3']
+
+nodes = list(map(lambda x: 'http://' + x, nodes_clean))
 
 for node in nodes:
-    r = requests.post(f'{node}:8000', data={})
+    r = requests.get(f'{node}:8000')
     print(r.status_code, r.reason)
 
 for node in nodes:
-    r = requests.post(f'{node}:8000/nodes/register', data={
-        "nodes": [node_inner for node_inner in nodes if node != node_inner]
+    print(f'{node}:8000/nodes/register')
+    print(str([node_inner + ':8000' for node_inner in nodes_clean if node[7:] != node_inner]).replace("'", '"'))
+    r = requests.post(f'{node}:8000/nodes/register', json={
+        "nodes": [node_inner + ':8000' for node_inner in nodes_clean if node[7:] != node_inner]
     })
     print(r.status_code, r.reason)
 
 while True:
-    node_id = random.randint(0, len(nodes))
-
+    node_id = random.randint(0, len(nodes)-1)
+    print(f'Running resolve on node {nodes[node_id]}')
     r = requests.get(f'{nodes[node_id]}:8000/nodes/resolve')
     print(r.status_code, r.reason)
     for i in range(random.randint(1, 5)):
-        r = requests.post(f'{nodes[node_id]}:8000/transactions/new', data=generate_transaction())
+        print(f'Running new transaction on node {nodes[node_id]}')
+        r = requests.post(f'{nodes[node_id]}:8000/transactions/new', json=generate_transaction())
         print(r.status_code, r.reason)
+
+    print(f'Running mine on node {nodes[node_id]}')
     r = requests.get(f'{nodes[node_id]}:8000/mine')
     print(r.status_code, r.reason)
     sleep(2)
